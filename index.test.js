539986bdbcbadb99e03fdd8f550d2fa7acc134c3
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2020-11-14 10:24:19 -0600

    Base file structure and complete assignment instructions in README. No tests.

---
+++ index.test.js
@@ -0,0 +1,11 @@
+require('jest');
+
+const { IPv4Address, IPv4Network } = require('./index');
+
+describe('IPv4Address', () => {
+  xit('needs tests', () => { });
+});
+
+describe('IPv4Network', () => {
+  xit('needs tests', () => { });
+});
