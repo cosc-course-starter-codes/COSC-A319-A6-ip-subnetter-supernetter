7a40b605f1597732557cbbc2a8153c826236a816
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2020-12-20 12:01:44 -0600

    Patch bad tests

--- index.test.js
+++ index.test.js
@@ -53,6 +53,12 @@ describe('IPv4Address', () => {
     });
   });
 
+  describe('static parseIPAddress', () => {
+    it('parses IP Addresses strings into binary values', () => {
+      const expected = 2502595227 >>> 0;
+      expect(IPv4Address.parseIPAddress('149.42.146.155')).toBe(expected);
+    });
+  });
   describe('static create', () => {
     it('should accept binary values', () => {
       const addr = IPv4Address.create(2502595227 >>> 0, 4294959104 >>> 0);
@@ -63,7 +69,7 @@ describe('IPv4Address', () => {
       expect(addr.subnetMask).toBe(4294959104 >>> 0);
     });
     it('should accept string IPv4 and subnet mask values', () => {
-      const addr = IPv4Address.create('149.42.146.155', '255.255.124.0');
+      const addr = IPv4Address.create('149.42.146.155', '255.255.224.0');
       expect(addr).toBeInstanceOf(IPv4Address);
       expect(addr).toBeInstanceOf(IPv4Address);
       expect(addr.networkID).toBe(2502590464 >>> 0);
@@ -82,7 +88,7 @@ describe('IPv4Address', () => {
     });
   });
 
-  describe('static display', () => {
+  describe('display', () => {
     let subject;
     beforeEach(() => {
       subject = new IPv4Address(2502595227 >>> 0, 4294959104 >>> 0);
@@ -193,7 +199,7 @@ describe('IPv4Network', () => {
       expect(net.hostAddressRange[1]).toBe(2502598654 >>> 0);
     });
     it('should accept string IPv4 and subnet mask values', () => {
-      const net = IPv4Network.create('149.42.146.155', '255.255.124.0');
+      const net = IPv4Network.create('149.42.146.155', '255.255.224.0');
       expect(net).toBeInstanceOf(IPv4Network);
       expect(net.networkID).toBe(2502590464 >>> 0);
       expect(net.prefixLength).toBe(19);
@@ -213,9 +219,9 @@ describe('IPv4Network', () => {
       expect(subnets).toBeInstanceOf(Array);
       expect(subnets.length).toBe(4);
       expect(subnets.map((net) => net.networkCIDRAddress)).toContain('149.42.128.0/21');
-      expect(subnets.map((net) => net.networkCIDRAddress)).toContain('149.42.134.0/21');
-      expect(subnets.map((net) => net.networkCIDRAddress)).toContain('149.42.142.0/21');
-      expect(subnets.map((net) => net.networkCIDRAddress)).toContain('149.42.150.0/21');
+      expect(subnets.map((net) => net.networkCIDRAddress)).toContain('149.42.136.0/21');
+      expect(subnets.map((net) => net.networkCIDRAddress)).toContain('149.42.144.0/21');
+      expect(subnets.map((net) => net.networkCIDRAddress)).toContain('149.42.152.0/21');
     });
     it('computes the correct properties for each subnet', () => {
       const subnets = subject.subnet(21);
@@ -231,17 +237,17 @@ describe('IPv4Network', () => {
       expect(net1.broadcastAddress).toBe(2502592511 >>> 0);
       expect(net1.hostAddressRange[0]).toBe(2502590465 >>> 0);
       expect(net1.hostAddressRange[1]).toBe(2502592510 >>> 0);
-      const net2 = subnets.filter(net => net.networkCIDRAddress === '149.42.134.0/21')[0];
+      const net2 = subnets.filter(net => net.networkCIDRAddress === '149.42.136.0/21')[0];
       expect(net2.networkID).toBe(2502592512 >>> 0);
       expect(net2.broadcastAddress).toBe(2502594559 >>> 0);
       expect(net2.hostAddressRange[0]).toBe(2502592513 >>> 0);
       expect(net2.hostAddressRange[1]).toBe(2502594558 >>> 0);
-      const net3 = subnets.filter(net => net.networkCIDRAddress === '149.42.142.0/21')[0];
+      const net3 = subnets.filter(net => net.networkCIDRAddress === '149.42.144.0/21')[0];
       expect(net3.networkID).toBe(2502594560 >>> 0);
       expect(net3.broadcastAddress).toBe(2502596607 >>> 0);
       expect(net3.hostAddressRange[0]).toBe(2502594561 >>> 0);
       expect(net3.hostAddressRange[1]).toBe(2502596606 >>> 0);
-      const net4 = subnets.filter(net => net.networkCIDRAddress === '149.42.150.0/21')[0];
+      const net4 = subnets.filter(net => net.networkCIDRAddress === '149.42.152.0/21')[0];
       expect(net4.networkID).toBe(2502596608 >>> 0);
       expect(net4.broadcastAddress).toBe(2502598655 >>> 0);
       expect(net4.hostAddressRange[0]).toBe(2502596609 >>> 0);
@@ -253,15 +259,15 @@ describe('IPv4Network', () => {
     let net1, net2, net3, net4;
     beforeEach(() => {
       net1 = IPv4Network.create('146.51.128.0/21');
-      net2 = IPv4Network.create('146.51.134.0/21');
-      net3 = IPv4Network.create('146.51.142.0/21');
-      net4 = IPv4Network.create('146.51.150.0/21');
+      net2 = IPv4Network.create('146.51.136.0/21');
+      net3 = IPv4Network.create('146.51.144.0/21');
+      net4 = IPv4Network.create('146.51.152.0/21');
     });
     it('returns an array of supernets covering the subnetworks', () => {
       const supernets = IPv4Network.supernet([net1, net4, net2]);
       expect(supernets).toBeInstanceOf(Array);
       expect(supernets.map((net) => net.networkCIDRAddress)).toContain('146.51.128.0/20');
-      expect(supernets.map((net) => net.networkCIDRAddress)).toContain('146.51.150.0/21');
+      expect(supernets.map((net) => net.networkCIDRAddress)).toContain('146.51.152.0/21');
     });
     it('returns a minimal array of supernets covering the subnetworks', () => {
       const supernets = IPv4Network.supernet([net1, net3, net2, net4]);
@@ -276,20 +282,20 @@ describe('IPv4Network', () => {
       expect(supernet1.subnetMask).toBe(4294963200 >>> 0);
       expect(supernet1.hostAddressRange).toBeInstanceOf(Array);
       expect(supernet1.hostAddressRange.length).toBe(2);
-      expect(supernet1.networkID).toBe(2502590464 >>> 0);
-      expect(supernet1.broadcastAddress).toBe(2502594559 >>> 0);
-      expect(supernet1.hostAddressRange[0]).toBe(502590465 >>> 0);
-      expect(supernet1.hostAddressRange[1]).toBe(2502594558 >>> 0);
-      const supernet2 = supernets.filter(net => net.networkCIDRAddress === '146.51.150.0/21')[0];
+      expect(supernet1.networkID).toBe(2452848640 >>> 0);
+      expect(supernet1.broadcastAddress).toBe(2452852735 >>> 0);
+      expect(supernet1.hostAddressRange[0]).toBe(2452848641 >>> 0);
+      expect(supernet1.hostAddressRange[1]).toBe(2452852734 >>> 0);
+      const supernet2 = supernets.filter(net => net.networkCIDRAddress === '146.51.152.0/21')[0];
       expect(supernet2).toBeInstanceOf(IPv4Network);
       expect(supernet2.prefixLength).toBe(21);
       expect(supernet2.subnetMask).toBe(4294965248 >>> 0);
       expect(supernet2.hostAddressRange).toBeInstanceOf(Array);
       expect(supernet2.hostAddressRange.length).toBe(2);
-      expect(supernet2.networkID).toBe(2502596608 >>> 0);
-      expect(supernet2.broadcastAddress).toBe(2502598655 >>> 0);
-      expect(supernet2.hostAddressRange[0]).toBe(2502596609 >>> 0);
-      expect(supernet2.hostAddressRange[1]).toBe(2502598654 >>> 0);
+      expect(supernet2.networkID).toBe(2452854784 >>> 0);
+      expect(supernet2.broadcastAddress).toBe(2452856831 >>> 0);
+      expect(supernet2.hostAddressRange[0]).toBe(2452854785 >>> 0);
+      expect(supernet2.hostAddressRange[1]).toBe(2452856830 >>> 0);
     });
   });
 });
